8c67777fbbeb9ab2aa542d9b5a5cafa77f0ba029
Maryna <maryna.sotnyk@finsa.it>
2024-10-11 22:51:48 +0200

    commit

---
+++ src/components/dotted-separator.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+
+interface DottedSeparatorProps {
+  className?: string;
+  color?: string;
+  height?: string;
+  dotSize?: string;
+  gapSize?: string;
+  direction?: "horizontal" | "vertical";
+}
